c91911bc5436952e881f86c573628694168e68cd
davidvargas <davidfv21@hotmail.com>
2019-05-13 21:23:46 -0500

    ,mbkv

---
+++ src/estructural/bridge/ElaborarLasagna.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructural.bridge;
+
+/**
+ *
+ * @author davidvargas
+ */
+public class ElaborarLasagna extends ElaborarAlimento
+{
+    public ElaborarLasagna( IElaborar implementador )
+    {
+        this.setImplementador( implementador );
+    }
+    // ------------------------
+    @Override
+     public void obtener()
+    {
+        System.out.println("Preparando lasagna...");
+         this.getImplementador().procesar();
+    }
+}
